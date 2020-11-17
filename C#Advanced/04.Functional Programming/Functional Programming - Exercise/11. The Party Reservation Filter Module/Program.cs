2647ad875574c53e6f2739403a4bd357084f2e05
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2020-11-17 11:10:17 +0200

    Full course

--- C#Advanced/04.Functional Programming/Functional Programming - Exercise/11. The Party Reservation Filter Module/Program.cs
+++ C#Advanced/04.Functional Programming/Functional Programming - Exercise/11. The Party Reservation Filter Module/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _11._The_Party_Reservation_Filter_Module
 {
@@ -6,7 +8,63 @@ namespace _11._The_Party_Reservation_Filter_Module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<string> filters = new List<string>();
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "Print")
+            {
+                var data = input
+                    .Split(";", StringSplitOptions.RemoveEmptyEntries);
+                var command = data[0];
+                var arr = data.Skip(1).ToArray();
+                if (command == "Add filter")
+                {
+                    filters.Add($"{arr[0]} {arr[1]}");
+                }
+                else if (command == "Remove filter")
+                {
+                    filters.Remove($"{arr[0]} {arr[1]}");
+                }
+            }
+
+            foreach (var filter in filters)
+            {
+                list = Filter(filter, list);
+            }
+            PrintResult(list);
+
         }
+        static Func<string, List<string>, List<string>> Filter = (filters, names) =>
+          {
+              List<string> listFiltered = new List<string>();
+              var filter = filters
+                           .Split(" ")
+                           .ToArray();
+              if (filter[0] == "Starts")
+              {
+                  listFiltered = names.Where(name => !name.StartsWith(filter[2])).ToList();
+              }
+              else if (filter[0] == "Ends")
+              {
+                  listFiltered = names.Where(name => !name.EndsWith(filter[2])).ToList();
+              }
+              else if (filter[0] == "Length")
+              {
+                  listFiltered = names.Where(name => name.Length != int.Parse(filter[1])).ToList();
+              }
+              else if (filter[0] == "Contains")
+              {
+                  listFiltered = names.Where(name => !name.Contains(filter[1])).ToList();
+              }
+              return listFiltered;
+          };
+        static Action<List<string>> PrintResult = names =>
+        {
+            Console.WriteLine(string.Join(" ", names));
+        };
+
+
     }
 }
---
+++ C#Advanced/04.Functional Programming/Functional Programming - Exercise/12. TriFunction/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._TriFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            Func<string, int, bool> checkSumOfName = (name, value) =>
+            {
+                int sumOfName = 0;
+                foreach (var character in name)
+                {
+                    sumOfName += character;
+                }
+                return sumOfName >= value;
+            };
+            Func<Func<string,int,bool>, List<string>, int, string> printName = (func, names, value) =>
+               {
+                   string win = string.Empty;
+                   foreach (var name in names)
+                   {
+                       if (func(name, value))
+                       {
+                           win = name;
+                           return win;
+                       }
+                   }
+                   return win;
+               };
+            Console.WriteLine(printName(checkSumOfName, names, number));
+        }
+
+    }
+    
+}
