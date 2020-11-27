d6495b5af337e50a5f4e015338159daf666656ce
Petroslav <petroslavivovgochev1996@abv.bg>
2020-11-27 09:51:51 +0200

    Full course

---
+++ C#Advanced/06.Generics/Generics - Exercise/09.CustomLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _09.CustomLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomList<string> list = new CustomList<string>();
+            list.Add("Pesho");
+            list.Add("Ivan");
+            list.Add("Mitko");
+            list.Add("Gosho");
+            list.Insert(4, "Rosko");
+            Console.WriteLine(list.Contains("Ivan"));
+            Console.WriteLine(list);
+        }
+    }
+}
