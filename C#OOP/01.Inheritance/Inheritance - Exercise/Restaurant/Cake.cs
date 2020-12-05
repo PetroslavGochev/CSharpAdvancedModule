bb993bf5c63e97ad693fb3d79ef2584a4cde28cf
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2020-12-05 10:28:51 +0200

    Exercise

---
+++ C#OOP/01.Inheritance/Inheritance - Exercise/Restaurant/Cake.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Cake : Dessert
+    {
+        private const double Grams = 250;
+        private const double Calories = 1000;
+        private const decimal Price = 5;
+        public Cake(string name) : base(name, Price, Grams, Calories)
+        {
+        }
+    }
+}
