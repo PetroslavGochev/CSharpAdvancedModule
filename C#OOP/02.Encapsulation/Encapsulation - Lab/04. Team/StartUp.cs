fe7f79c17cccd7c22811921f6cde40297cd087ed
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2020-12-14 09:11:58 +0200

    Encapsulation

---
+++ C#OOP/02.Encapsulation/Encapsulation - Lab/04. Team/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var persons = new List<Person>();
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                Person person = null;
+                try
+                {
+                    person = new Person(cmdArgs[0],
+                                       cmdArgs[1],
+                                       int.Parse(cmdArgs[2]),
+                                       decimal.Parse(cmdArgs[3]));
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                persons.Add(person);
+            }
+
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in persons)
+            {
+                team.AddPlayer(person);
+            }
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+        }
+    }
+}
+
