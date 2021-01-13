1056e211ecbba9bb561358a5e815eae914264abc
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-01-13 21:18:34 +0200

    Update

---
+++ C#OOP/07.ReflectionAndAttributes/ReflectionAndAttributes - Lab/ReflectionAndAttributesLab/Stealer/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+       
+            Spy spy = new Spy();
+            ////First Exercise
+            //string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+            //Console.WriteLine(result);
+            //Second Exercise
+            //string result = spy.AnalyzeAcessModifiers("Stealer.Hacker");
+            //Console.WriteLine(result);
+            // Third Exercise
+            //string result = spy.RevealPrivateMethods("Stealer.Hacker");
+            //Console.WriteLine(result);
+            // Fourth Exercise
+            //string result = spy.GetAllMethodsAndRecognize("Stealer.Hacker");
+            //Console.WriteLine(result);
+        }
+    }
+}
