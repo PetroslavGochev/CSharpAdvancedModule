bbb17f1c3422b2e7e1338d2e327d7c7353c0565d
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-02-20 12:14:17 +0200

    Advanced Module

---
+++ Advanced Module C#/10.Exam Preparation/2019April16/01.TrojanInvasion/StartUp.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.TrojanInvasion
+{
+    class StartUp
+    {
+        private const string PLATE_WINNER = "The Spartans successfully repulsed the Trojan attack.";
+        private const string WARRIOR_WINNER = "The Trojans successfully destroyed the Spartan defense.";
+
+        private static Stack<int> plate = new Stack<int>();
+        private static Stack<int> warrior = new Stack<int>();
+        static void Main(string[] args)
+        {
+            CreateTrojanAndSpartans();
+            PrintWinner();
+        }
+        private static void CreateTrojanAndSpartans()
+        {
+            int numberOfWaves = int.Parse(Console.ReadLine());
+            int[] trojanPlate = ReadDataFromConsole();
+            EnqueueTrojansInQueue(trojanPlate);
+            for (int i = 1; i <= numberOfWaves; i++)
+            {
+                int[] spartansArgs = ReadDataFromConsole();
+                if (i % 3 == 0)
+                {
+                    trojanPlate = ReadDataFromConsole();
+                    EnqueueTrojansInQueue(trojanPlate);
+                }
+                if (plate.Count == 0)
+                {
+                    break;
+                }
+                foreach (var s in spartansArgs)
+                {
+                    warrior.Push(s);
+                }
+                Battle();
+            }
+        }
+        private static void PrintWinner()
+        {
+            Console.WriteLine(Winner());
+            if (plate.Count > 0)
+            {
+                PrintRestValue(plate, "Plates");
+            }
+            else
+            {
+                PrintRestValue(warrior, "Warriors");
+            }
+        }
+        private static void PrintRestValue(Stack<int> args,string winner)
+        {
+            Console.Write($"{winner} left: ");
+            while (args.Count != 0)
+            {
+                if(args.Count == 1)
+                {
+                    Console.Write($"{args.Pop()}");
+                    break;
+                }
+                Console.Write($"{args.Pop()}, ");
+            }
+            Console.WriteLine();
+        }
+        private static string Winner()
+         => plate.Count > 0 ? PLATE_WINNER : WARRIOR_WINNER;
+        private static void Battle()
+        {
+            while (warrior.Count != 0 && plate.Count != 0)
+            {
+                int compare = plate.Peek().CompareTo(warrior.Peek());
+                if (compare > 0)
+                {
+                    plate.Push(plate.Pop() - warrior.Pop());
+                }
+                else if (compare == 0)
+                {
+                    plate.Pop();
+                    warrior.Pop();
+                }
+                else
+                {
+                    warrior.Push(warrior.Pop() - plate.Pop());
+                }
+            }
+        }
+        private static void EnqueueTrojansInQueue(int[] args)
+        {
+            for (int i = args.Length - 1; i >= 0; i--)
+            {
+                plate.Push(args[i]);
+            }
+        }
+        private static int[] ReadDataFromConsole()
+            => Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+    }
+}
---
+++ Advanced Module C#/10.Exam Preparation/2019April16/02.HelensAbduction/StartUp.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.HelensAbduction
+{
+    class StartUp
+    {
+        private const string PARIS_DIED = "Paris died at {0};{1}.";
+        private const string PARIS_SAVE_HELENA = "Paris has successfully abducted Helen! Energy left: {0}";
+
+        private static bool IsFindHelena = false;
+        private static int energy = 0;
+        private static int parisRow = 0;
+        private static int parisCol = 0;
+        private static char[][] matrix;
+        private static string direction = string.Empty;
+        private static int enemyRow;
+        private static int enemyCol;
+        static void Main(string[] args)
+        {
+            ReceiveDataAndCreateMatrix();
+            Console.WriteLine(ReceiveDirection());
+            PrintMatrix();
+        }
+        private static void ReceiveDataAndCreateMatrix()
+        {
+            energy = int.Parse(Console.ReadLine());
+            int numberOfRow = int.Parse(Console.ReadLine());
+            matrix = new char[numberOfRow][];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var data = Console.ReadLine();
+                matrix[row] = new char[data.Length];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (data[col] == 'P')
+                    {
+                        parisRow = row;
+                        parisCol = col;
+                    }
+                    matrix[row][col] = data[col];
+                }
+            }
+        }
+
+        private static string ReceiveDirection()
+        {
+            while (energy > 0 && !IsFindHelena)
+            {
+                string[] command =
+                    Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                direction = command[0];
+                enemyRow = int.Parse(command[1]);
+                enemyCol = int.Parse(command[2]);
+                matrix[enemyRow][enemyCol] = 'S';
+                energy--;
+                try
+                {
+                    matrix[parisRow][parisCol] = '-';
+                    if (direction == "up")
+                    {
+                        CheckIndexIsOutOfTheRange(parisRow - 1, parisCol);
+                        parisRow--;
+                    }
+                    else if (direction == "down")
+                    {
+                        CheckIndexIsOutOfTheRange(parisRow + 1, parisCol);
+                        parisRow++;
+                    }
+                    else if (direction == "left")
+                    {
+                        CheckIndexIsOutOfTheRange(parisRow, parisCol - 1);
+                        parisCol--;
+                    }
+                    else if (direction == "right")
+                    {
+                        CheckIndexIsOutOfTheRange(parisRow, parisCol + 1);
+                        parisCol++;
+                    }
+                    Movement();
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    matrix[parisRow][parisCol] = 'P';
+                    continue;
+                }
+            }
+            if (IsFindHelena)
+            {
+                matrix[parisRow][parisCol] = '-';
+                return String.Format(PARIS_SAVE_HELENA, energy);
+            }
+            else
+            {
+                matrix[parisRow][parisCol] = 'X';
+                return String.Format(PARIS_DIED, parisRow, parisCol);
+            }
+        }
+
+        private static char CheckIndexIsOutOfTheRange(int row, int col)
+            => matrix[row][col];
+
+        private static void Movement()
+        {
+            if (matrix[parisRow][parisCol] == 'S')
+            {
+                energy -= 2;
+            }
+            else if (matrix[parisRow][parisCol] == 'H')
+            {
+                IsFindHelena = true;
+            }
+        }
+
+        private static void PrintMatrix()
+        {
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+    }
+}
