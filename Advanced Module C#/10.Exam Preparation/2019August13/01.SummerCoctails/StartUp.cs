43bd4e3b44a3c3783eca998177fbc26751e2ed4b
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-03-01 18:40:00 +0200

    Advanced Module

---
+++ Advanced Module C#/10.Exam Preparation/2019August13/01.SummerCoctails/StartUp.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.SummerCoctails
+{
+    class StartUp
+    {
+        private const string PARTY_TIME = "It's party time! The cocktails are ready!";
+        private const string NO_PARTY = "What a pity! You didn't manage to prepare all cocktails.";
+        private const string COCKTAIL = " # {0} --> {1}";
+
+
+        private static IDictionary<int, string> coctail = new Dictionary<int, string>()
+            {
+                {150,"Mimosa"},
+                {250,"Daiquiri"},
+                {300,"Sunshine"},
+                {400,"Mojito"}
+            };
+        private static IDictionary<string, int> countOfCoctail = new Dictionary<string, int>()
+            {
+                {"Mimosa",0},
+                {"Daiquiri",0},
+                {"Sunshine",0},
+                {"Mojito",0}
+            };
+        private static Queue<int> ingredient = new Queue<int>();
+        private static Stack<int> freshLevel = new Stack<int>();
+        static void Main(string[] args)
+        {
+            var ingredientsArgs = ReadDataFromConsole();
+            foreach (var i in ingredientsArgs)
+            {
+                ingredient.Enqueue(i);
+            }
+            var freshArgs = ReadDataFromConsole();
+            foreach (var f in freshArgs)
+            {
+                freshLevel.Push(f);
+            }
+            Combination();
+            Console.WriteLine(ReturnResult());
+            PrintIngredient();
+            Console.WriteLine(PrintCoctails());
+            
+        }
+        private static string PrintCoctails()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in countOfCoctail
+                                            .Where(x=>x.Value != 0)
+                                            .OrderBy(x=>x.Key))
+            {
+                sb.AppendLine(String.Format(COCKTAIL, item.Key, item.Value));
+            }
+            return sb.ToString().TrimEnd();
+        }
+        private static void PrintIngredient()
+        {
+            int sum = 0;
+            while(ingredient.Count != 0)
+            {
+                sum += ingredient.Dequeue();
+            }
+            if(sum != 0)
+            {
+                Console.WriteLine($"Ingredients left: {sum}");
+            }
+        }
+        private static string ReturnResult()
+            => countOfCoctail.Any(x => x.Value == 0)
+            ? NO_PARTY
+            : PARTY_TIME;
+        private static void Combination()
+        {
+            while (IsNotEmpty())
+            {
+                if(ingredient.Peek() == 0)
+                {
+                    ingredient.Dequeue();
+                    continue;
+                }
+                if (IsSumEqual())
+                {
+                    countOfCoctail[coctail[ingredient.Dequeue() * freshLevel.Pop()]]++;
+                }
+                else
+                {
+                    ingredient.Enqueue(ingredient.Dequeue() + 5);
+                    freshLevel.Pop();
+                }
+            }
+        }
+        private static bool IsSumEqual()
+            => coctail.ContainsKey(ingredient.Peek() * freshLevel.Peek());
+        private static bool IsNotEmpty()
+            => ingredient.Count != 0 && freshLevel.Count != 0;
+        private static int[] ReadDataFromConsole()
+            => Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+    }
+}
---
+++ Advanced Module C#/10.Exam Preparation/2019August13/02.SeashellTreasure/StartUp.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SeashellTreasure
+{
+    class StartUp
+    {
+        private const string COLLECTION = "Collected seashells: {0} -> ";
+        private const string STOLEN = "Stolen seashells: {0}";
+        private const string FINISH = "Sunset";
+
+        private static List<char> collect = new List<char>();
+        private static List<char> stolen = new List<char>();
+        private static char[][] matrix;
+        private static string[] command;
+        private static int row;
+        private static int column;
+        private static string direction;
+        static void Main(string[] args)
+        {
+            CreateMatrix();
+            ReceiveCommandFromConsole();
+            PrintCollectionAndSteal();
+            PrintMatrix();
+        }
+        private static void PrintCollectionAndSteal()
+        {
+            Console.Write(String.Format(COLLECTION, collect.Count));
+            if(collect.Count > 0)
+            {
+                Console.WriteLine(string.Join(", ", collect));
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+            Console.WriteLine(String.Format(STOLEN, stolen.Count));
+        }
+        private static void PrintMatrix()
+        {
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(String.Join(" ", row));
+            }
+        }
+        private static void CreateMatrix()
+        {
+            int numberOfRows = int.Parse(Console.ReadLine());
+            matrix = new char[numberOfRows][];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var data = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                matrix[row] = new char[data.Length];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    matrix[row][col] = char.Parse(data[col]);
+                }
+            }
+        }
+
+        private static void ReceiveCommandFromConsole()
+        {
+            string data = string.Empty;
+            while ((data = Console.ReadLine()) != FINISH)
+            {
+                command = data
+                   .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                   .ToArray();
+                row = int.Parse(command[1]);
+                column = int.Parse(command[2]);
+                try
+                {
+                    if (IsCommandCollect())
+                    {
+                        Collect();
+                    }
+                    else
+                    {
+                        direction = command[3];
+                        Steal();
+                    }
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    continue;
+                }
+               
+            }
+        }
+
+        private static void Collect()
+        {
+            if (matrix[row][column] != '-')
+            {
+                collect.Add(matrix[row][column]);
+                matrix[row][column] = '-';
+            }
+        }
+
+        private static void Steal()
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                if (matrix[row][column] != '-')
+                {
+                    stolen.Add(matrix[row][column]);
+                    matrix[row][column] = '-';
+                }
+                if (direction == "up")
+                {
+                    row--;
+                }
+                else if (direction == "down")
+                {
+                    row++;
+                }
+                else if (direction == "left")
+                {
+                    column--;
+                }
+                else if (direction == "right")
+                {
+                    column++;
+                }
+            }            
+        }
+        private static bool IsCommandCollect()
+            => command[0] == "Collect";
+    }
+}
