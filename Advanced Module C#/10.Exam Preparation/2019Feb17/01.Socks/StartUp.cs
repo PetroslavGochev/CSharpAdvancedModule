8f5a2b121734d7eee078925058d995adc1b0b6dc
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-03-06 16:06:33 +0200

    Advanced Module

---
+++ Advanced Module C#/10.Exam Preparation/2019Feb17/01.Socks/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Socks
+{
+    class StartUp
+    {
+        private static List<int> pairs = new List<int>();
+        private static Stack<int> leftSock = new Stack<int>();
+        private static Queue<int> rightSock = new Queue<int>();
+        static void Main(string[] args)
+        {
+            var leftArgs = ReadSocskArgsFromConsole();
+            foreach (var ls in leftArgs)
+            {
+                leftSock.Push(ls);
+            }
+            var rightArgs = ReadSocskArgsFromConsole();
+            foreach (var rs in rightArgs)
+            {
+                rightSock.Enqueue(rs);
+            }
+            Combination();
+            Console.WriteLine(pairs.Max());
+            Console.WriteLine(String.Join(" ", pairs));
+        }
+        private static void Combination()
+        {
+            while (IsTrue())
+            {
+                int areEqual = leftSock.Peek().CompareTo(rightSock.Peek());
+                if (areEqual == 1)
+                {
+                    pairs.Add(leftSock.Pop() + rightSock.Dequeue());
+                }
+                else if (areEqual == 0)
+                {
+                    leftSock.Push(leftSock.Pop() + 1);
+                    rightSock.Dequeue();
+                }
+                else
+                {
+                    leftSock.Pop();
+                }
+            }
+        }
+        private static bool IsTrue()
+            => leftSock.Count != 0 && rightSock.Count != 0;
+        private static int[] ReadSocskArgsFromConsole()
+            => Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+    }
+}
---
+++ Advanced Module C#/10.Exam Preparation/2019Feb17/02.ExelFunction/StartUp.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ExelFunction
+{
+    class StartUp
+    {
+        private static string[] command;
+        private static List<string> firstRow = new List<string>();
+        private static string[][] matrix;
+        static void Main(string[] args)
+        {
+            CreateMatrix();
+            ReceiveCommand();
+
+        }
+
+        private static void ReceiveCommand()
+        {
+            command = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            if (command[0] == "filter")
+            {
+                FilterResultInMatrix();
+            }
+            else if (command[0] == "hide")
+            {
+                HideResultInMatrix();
+            }
+            else if (command[0] == "sort")
+            {
+                SortByColumn();
+            }
+        }
+        private static void SortByColumn()
+        {
+            string filterByCol = command[1];
+            int col = firstRow.FindIndex(x => x.Contains(filterByCol));
+            List<string> sortedColumn = new List<string>();
+            PrintRow(firstRow.ToArray());
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                sortedColumn.Add(matrix[row][col]);
+            }
+            sortedColumn = sortedColumn.OrderBy(x => x).ToList();
+            for (int i = 0; i < sortedColumn.Count; i++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    if (matrix[row][col] == sortedColumn[i])
+                    {
+                        PrintRow(matrix[row]);
+                    }
+                }
+            }
+        }
+        private static void HideResultInMatrix()
+        {
+            string filterByCol = command[1];
+            int col = firstRow.FindIndex(x => x.Contains(filterByCol));
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                List<string> matrixRow = matrix[row].ToList();
+                matrixRow.RemoveAt(col);
+                PrintRow(matrixRow.ToArray());
+            }
+        }
+        private static void FilterResultInMatrix()
+        {
+            string filterByCol = command[1];
+            string filterValue = command[2];
+            int col = firstRow.FindIndex(x => x.Contains(filterByCol));
+            PrintRow(firstRow.ToArray());
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (matrix[row][col] == filterValue)
+                {
+                    PrintRow(matrix[row]);
+                }
+            }
+        }
+        private static void PrintRow(string[] row)
+            => Console.WriteLine(string.Join(" | ", row));
+        private static void CreateMatrix()
+        {
+            int numberOfRows = int.Parse(Console.ReadLine());
+
+            matrix = new string[numberOfRows][];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var data = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                matrix[row] = new string[data.Length];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (row == 0)
+                    {
+                        firstRow.Add(data[col]);
+                    }
+                    matrix[row][col] = data[col];
+                }
+            }
+        }
+    }
+}
