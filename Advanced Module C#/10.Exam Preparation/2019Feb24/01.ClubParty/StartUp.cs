8c607ae57f30482d99b113e4d8f2eb6d7a7f54ef
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-03-07 09:53:46 +0200

    Advanced Module

---
+++ Advanced Module C#/10.Exam Preparation/2019Feb24/01.ClubParty/StartUp.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ClubParty
+{
+    class StartUp
+    {
+        private const string PRINT_HALL = "{0} -> ";
+
+        private static int maxCapacity;
+        private static Dictionary<char, List<int>> halls = new Dictionary<char, List<int>>();
+        private static Stack<string> input = new Stack<string>();
+        static void Main(string[] args)
+        {
+            maxCapacity = int.Parse(Console.ReadLine());
+            var inputArgs = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .ToArray();
+            foreach (var item in inputArgs)
+            {
+                input.Push(item);
+            }
+            FillHall();
+        }
+        private static void FillHall()
+        {
+            while (IsTrue())
+            {
+                if (input.Peek().Length < 2 && IsLetter())
+                {
+                    halls.Add(char.Parse(input.Pop()), new List<int>());
+                }
+                else
+                {
+                    if (halls.Count == 0)
+                    {
+                        input.Pop();
+                    }
+                    else
+                    {
+                        AddReservationInHall();
+                    }
+                }
+            }
+        }
+        private static void AddReservationInHall()
+        {
+            var hal = halls.First();
+            if (hal.Value.Sum() + int.Parse(input.Peek()) < maxCapacity)
+            {
+                halls[hal.Key].Add(int.Parse(input.Pop()));
+            }
+            else
+            {
+                PrintHall();
+            }
+        }
+        private static void PrintHall()
+        {
+            int currentReservation = int.Parse(input.Peek());
+
+            var hal = halls.First();
+            if (hal.Value.Sum() + currentReservation == maxCapacity)
+            {
+                halls[hal.Key].Add(int.Parse(input.Pop()));
+            }
+            else
+            {
+                input.Pop();
+            }
+            Console.Write(String.Format(PRINT_HALL, hal.Key));
+            for (int i = 0; i < hal.Value.Count; i++)
+            {
+                if (i == hal.Value.Count - 1)
+                {
+                    Console.WriteLine($"{hal.Value[i]}");
+                    break;
+                }
+                Console.Write($"{hal.Value[i]}, ");
+            }
+            halls.Remove(hal.Key);
+            if (halls.Count != 0)
+            {
+                input.Push(currentReservation.ToString());
+                AddReservationInHall();
+            }
+
+        }
+
+        private static bool IsLetter() => char.IsLetter(char.Parse(input.Peek()));
+        private static bool IsTrue() => input.Count != 0;
+    }
+}
---
+++ Advanced Module C#/10.Exam Preparation/2019Feb24/02.TronRacers/StartUp.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.TronRacers
+{
+    class StartUp
+    {
+        private static int firstRow;
+        private static int firstCol;
+        private static int secondRow;
+        private static int secondCol;
+        private static char[][] matrix;
+        static void Main(string[] args)
+        {
+            CreateMatrix();
+            ReceiveCommand();
+        }
+
+        private static void ReceiveCommand()
+        {
+            try
+            {
+                while (true)
+                {
+                    string[] command = Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .ToArray();
+                    string firstPlayer = command[0];
+                    string secondPlayer = command[1];
+                    PlayerAction('f', firstPlayer);
+                    PlayerAction('s', secondPlayer);
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                PrintMatrix();
+            }
+
+
+        }
+        private static void PlayerAction(char player, string command)
+        {
+            if (command == "down")
+            {
+                if (player == 'f')
+                {
+                    firstRow = DownDirection(player, firstRow, firstCol);
+                }
+                else
+                {
+                    secondRow = DownDirection(player, secondRow, secondCol);
+                }
+            }
+            else if (command == "up")
+            {
+                if (player == 'f')
+                {
+                    firstRow = UpDirection(player, firstRow, firstCol);
+                }
+                else
+                {
+                    secondRow = UpDirection(player, secondRow, secondCol);
+                }
+            }
+            else if (command == "left")
+            {
+                if (player == 'f')
+                {
+                    firstCol = LeftDirection(player, firstRow, firstCol);
+                }
+                else
+                {
+                    secondCol = LeftDirection(player, secondRow, secondCol);
+                }
+            }
+            else if (command == "right")
+            {
+                if (player == 'f')
+                {
+                    firstCol = RightDirection(player, firstRow, firstCol);
+                }
+                else
+                {
+                    secondCol = RightDirection(player, secondRow, secondCol);
+                }
+            }
+
+        }
+        private static int DownDirection(char player, int row, int col)
+        {
+            row++;
+            if (IsOutOfTheRange(row, col))
+            {
+                row = 0;
+            }
+            if (matrix[row][col] == '*')
+            {
+                matrix[row][col] = player;
+            }
+            else
+            {
+                matrix[row][col] = 'x';
+                matrix[-1][col] = 'E';
+            }
+            return row;
+        }
+        private static int UpDirection(char player, int row, int col)
+        {
+            row--;
+            if (IsOutOfTheRange(row, col))
+            {
+                row = matrix.GetLength(0) - 1;
+            }
+            if (matrix[row][col] == '*')
+            {
+                matrix[row][col] = player;
+            }
+            else
+            {
+                matrix[row][col] = 'x';
+                matrix[-1][col] = 'E';
+            }
+            return row;
+        }
+        private static int LeftDirection(char player, int row, int col)
+        {
+            col--;
+            if (IsOutOfTheRange(row, col))
+            {
+                col = matrix[row].Length - 1;
+            }
+            if (matrix[row][col] == '*')
+            {
+                matrix[row][col] = player;
+            }
+            else
+            {
+                matrix[row][col] = 'x';
+                matrix[-1][col] = 'E';
+            }
+            return col;
+        }
+        private static int RightDirection(char player, int row, int col)
+        {
+            col++;
+            if (IsOutOfTheRange(row, col))
+            {
+                col = 0;
+            }
+            if (matrix[row][col] == '*')
+            {
+                matrix[row][col] = player;
+            }
+            else
+            {
+                matrix[row][col] = 'x';
+                matrix[-1][col] = 'E';
+            }
+            return col;
+        }
+
+        private static bool IsOutOfTheRange(int row, int col)
+            => row < 0 || row >= matrix.GetLength(0) ||
+                col < 0 || col >= matrix[row].Length;
+        private static void CreateMatrix()
+        {
+            int numberOfRow = int.Parse(Console.ReadLine());
+            matrix = new char[numberOfRow][];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var data = Console.ReadLine();
+                matrix[row] = new char[data.Length];
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (data[col] == 'f')
+                    {
+                        firstRow = row;
+                        firstCol = col;
+                    }
+                    else if (data[col] == 's')
+                    {
+                        secondRow = row;
+                        secondCol = col;
+                    }
+                    matrix[row][col] = data[col];
+                }
+            }
+        }
+
+        private static void PrintMatrix()
+        {
+            foreach (var row in matrix)
+            {
+                Console.WriteLine(string.Join("", row));
+            }
+        }
+    }
+}
