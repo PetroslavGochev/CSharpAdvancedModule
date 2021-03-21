5176c6822b2b1e94a4ea3e1699e7aba152afb4a6
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-03-21 22:09:39 +0200

    Advanced Module

---
+++ Advanced Module C#/10.Exam Preparation/2019October26/01.DatingApp/StartUp.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.DatingApp
+{
+    class StartUp
+    {
+        private const string MATCHES = "Matches: {0}";
+        private const string MALE = "Males left: ";
+        private const string FEMALE = "Females left: ";
+
+        private static int matchesCouple = 0;
+        private static Queue<int> female = new Queue<int>();
+        private static Stack<int> male = new Stack<int>();
+        static void Main(string[] args)
+        {
+            var maleArgs = ReadDataFromConsole();
+            foreach (var m in maleArgs)
+            {
+                male.Push(m);
+            }
+            var femaleArgs = ReadDataFromConsole();
+            foreach (var f in femaleArgs)
+            {
+                female.Enqueue(f);
+            }
+            
+            MatchCouple();
+            PrintResult();
+        }
+        private static void PrintResult()
+        {
+            Console.WriteLine(String.Format(MATCHES, matchesCouple));
+            if(male.Count == 0)
+            {
+                Console.WriteLine($"{MALE}none");
+            }
+            else
+            {
+                Console.Write($"{MALE}");
+                while(male.Count != 0)
+                {
+                    if(male.Count == 1)
+                    {
+                        Console.WriteLine($"{male.Pop()}");
+                        break;
+                    }
+                    Console.Write($"{male.Pop()}, ");
+                }
+            }
+            if (female.Count == 0)
+            {
+                Console.WriteLine($"{FEMALE}none");
+            }
+            else
+            {
+                Console.Write($"{FEMALE}");
+                while (female.Count != 0)
+                {
+                    if (female.Count == 1)
+                    {
+                        Console.WriteLine($"{female.Dequeue()}");
+                        break;
+                    }
+                    Console.Write($"{female.Dequeue()}, ");
+                }
+            }
+        }
+        private static void MatchCouple()
+        {
+            while (IsTrue())
+            {
+                if (ValueIsNullOrBelow(female.Peek()))
+                {
+                    female.Dequeue();
+                    continue;
+                }
+                if (ValueIsNullOrBelow(male.Peek()))
+                {
+                    male.Pop();
+                    continue;
+                }
+                if (DivisbleBy25WithoutReminder(female.Peek()))
+                {
+                    female.Dequeue();
+                    if(female.Count >= 1)
+                    {
+                        female.Dequeue();
+                    }
+                    continue;
+                }
+                if (DivisbleBy25WithoutReminder(male.Peek()))
+                {
+                    male.Pop();
+                    if(male.Count >= 1)
+                    {
+                        male.Pop();
+                    }
+                    continue;
+                }
+                if (IsEqual())
+                {
+                    matchesCouple++;
+                    male.Pop();
+                    female.Dequeue();
+                }
+                else
+                {
+                    female.Dequeue();
+                    male.Push(male.Pop() - 2);
+                }
+            }
+            
+        }
+        private static bool DivisbleBy25WithoutReminder(int value)
+            => value % 25 == 0;
+        private static bool ValueIsNullOrBelow(int value)
+            => value <= 0;
+        private static bool IsTrue()
+            => female.Count != 0 && male.Count != 0;
+        private static bool IsEqual()
+            => female.Peek() == male.Peek();
+        private static int[] ReadDataFromConsole()
+            => Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+    }
+}
---
+++ Advanced Module C#/10.Exam Preparation/2019October26/02.BookWorm/StartUp.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+
+namespace _02.BookWorm
+{
+    class StartUp
+    {
+        private static StringBuilder initialString = new StringBuilder();
+        private static char[,] matrix;
+        private static int playerRow = 0;
+        private static int playerCol = 0;
+        static void Main(string[] args)
+        {
+            initialString.Append(Console.ReadLine());
+            CreateMatrix();
+            ReceiveCommandFromConsole();
+            Console.WriteLine(initialString);
+            PrintMatrix();
+        }
+        private static void ReceiveCommandFromConsole()
+        {
+            string command = string.Empty;
+            while((command = Console.ReadLine()) != "end")
+            {
+                try
+                {
+                    matrix[playerRow, playerCol] = '-';
+                    if(command == "down")
+                    {
+                        char check = matrix[playerRow + 1, playerCol];
+                        playerRow++;
+                    }
+                    else if(command == "up")
+                    {
+                        char check = matrix[playerRow - 1, playerCol];
+                        playerRow--;
+                    }
+                    else if(command == "left")
+                    {
+                        char check = matrix[playerRow , playerCol - 1];
+                        playerCol--;
+                    }
+                    else if(command == "right")
+                    {
+                        char check = matrix[playerRow , playerCol + 1];
+                        playerCol++;
+                    }
+                    Direction();
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    initialString.Remove(initialString.Length - 1, 1);                  
+                }
+                matrix[playerRow, playerCol] = 'P';
+            }
+        }
+        private static void Direction()
+        {
+            if(matrix[playerRow,playerCol] != '-')
+            {
+                initialString.Append(matrix[playerRow, playerCol]);
+            }
+        }
+        private static void CreateMatrix()
+        {
+            int numberOfRows = int.Parse(Console.ReadLine());
+            matrix = new char[numberOfRows,numberOfRows];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var data = Console.ReadLine();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if(data[col] == 'P')
+                    {
+                        playerRow = row;
+                        playerCol = col;
+                    }
+                    matrix[row, col] = data[col];
+                }
+            }
+        }
+
+        private static void PrintMatrix()
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
