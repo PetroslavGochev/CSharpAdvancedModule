8df4d1f027b0d4431446a55622c13bdd09e9709b
Petroslav Gochev <petroslavivovgochev1996@gmail.com>
2021-04-02 13:56:59 +0300

    OOP Module

---
+++ OOP Module C#/09.ExamPreparation/2020April12/Robot/Robots.Tests/RobotsTests.cs
@@ -0,0 +1,70 @@
+﻿namespace Robots.Tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Linq;
+
+    public class RobotsTests
+    {
+        private Robot robot;
+        private RobotManager robotManager;
+        [SetUp]
+        public void Setup()
+        {
+            this.robot = new Robot("Test", 50);
+            this.robotManager = new RobotManager(1);
+            this.robotManager.Add(this.robot);
+        }
+         
+        [Test]
+        public void PropertyOfRobotClass()
+        {
+            var expectedResult = this.robot.MaximumBattery;
+            Assert.AreEqual(expectedResult, 50);
+        }
+
+        [Test]
+        public void PropertyRobotManager()
+        {
+            var expectedResult = this.robotManager.Count;
+            var expectedCapacity = this.robotManager.Capacity;
+            Assert.AreEqual(expectedResult, 1);
+            Assert.AreEqual(expectedCapacity, 1);
+        }
+        [Test]
+        public void RemoveMethodWorked()
+        {
+            var expectedResult = 0;
+            this.robotManager.Remove("Test");
+            Assert.AreEqual(expectedResult,this.robotManager.Count);
+        }
+        [Test]
+        public void WorkMethod()
+        {
+            var expectedResult = 40;
+            this.robotManager.Work("Test","Test",10);
+            Assert.AreEqual(expectedResult, this.robot.Battery);
+        }
+        [Test]
+        public void ChargeMethod()
+        {
+            var expectedResult = 50;
+            this.robotManager.Charge("Test");
+            Assert.AreEqual(expectedResult, this.robot.Battery);
+        }
+
+        [Test]
+        public void InvalidCommand()
+        {
+            Assert.Throws<ArgumentException>(() => this.robotManager = new RobotManager(-1));
+            Assert.Throws<InvalidOperationException>(() => this.robotManager.Add(this.robot));
+            Assert.Throws<InvalidOperationException>(() => this.robotManager.Add(new Robot("Invalid",50)));
+            Assert.Throws<InvalidOperationException>(() => this.robotManager.Remove("Invalid"));
+            Assert.Throws<InvalidOperationException>(() => this.robotManager.Work("Invalid","Invalid",20));
+            Assert.Throws<InvalidOperationException>(() => this.robotManager.Work("Test","Test",60));
+            Assert.Throws<InvalidOperationException>(() => this.robotManager.Charge("Invalid"));
+
+        }
+
+    } 
+}
